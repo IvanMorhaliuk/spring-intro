cad69575a7e777d602b4e34a2675b307b48b821d
Ivan Morhaliuk <ivan.morhaliuk@gmail.com>
2022-01-12 15:52:41 +0100

    second-task

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,17 +1,31 @@
 package com.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//        MessageService messageService = applicationContext.getBean("messageService2", MessageService.class);
+//
+//        System.out.println(messageService.getMessage());
+//
+//
+//        applicationContext.close();
 
-        MessageService messageService = applicationContext.getBean("messageService2", MessageService.class);
+        //ZADANIE 2
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(messageServiceConfig.class);
+
+        MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
+
 
         System.out.println(messageService.getMessage());
 
 
-        applicationContext.close();
+
     }
 }
--- src/main/java/com/example/messageServiceConfig.java
+++ src/main/java/com/example/messageServiceConfig.java
@@ -2,11 +2,19 @@ package com.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class messageServiceConfig {
-    @Bean
-    public MessageService messageService(){
+    @Bean(name="messageService")
+    @Scope("prototype")
+    public MyNameMessageService getMyNameMessageService() {
         return new MyNameMessageService();
+    }
+
+    @Bean(name="messageService2")
+    @Scope("prototype")
+    public RandomTextMessageService getRandomTextMessageService() {
+        return new RandomTextMessageService();
     }
 }
